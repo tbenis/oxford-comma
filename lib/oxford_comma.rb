30164cadd1e62ff4f87df60b2d13db6a21388958
Benis Tambe <will.jenifer@yahoo.com>
2021-01-14 20:57:38 -0500

    commit tree 1075e7b00d1f6398de3ae6e4dadc9999f38478e3

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+    newStr = ""
+
+    if array.length > 1 && array.length < 3
+        lastitem = array.pop()
+        newStr = newStr + array.join(", ") + " and " + lastitem
+    elsif array.length >= 3
+        lastitem = array.pop()
+        newStr = newStr + array.join(", ") + ", and " + lastitem
+    else
+        newStr = newStr + array.join(", ")
+    end
+    
+    return newStr
 
 end
